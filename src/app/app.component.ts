00f465ace9846079c55a5fab210626c162e51d7b
Rina Andriana <rina.andriana@knowit.no>
2017-11-27 23:13:11 +0100

    a lot of changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  doBlock = false;
+  minViewportSize = 650;
+
+  ngOnInit() {
+    let viewportWidth = window.innerWidth;
+
+    if ( viewportWidth < this.minViewportSize ) {
+      this.doBlock = true;
+    }
+
+    this.doRunMobileBlocker();
+  }
+
+  doRunMobileBlocker(): void {
+    window.onresize = () => {
+      if ( window.innerWidth < this.minViewportSize ) {
+        this.doBlock = true;
+      } else {
+        this.doBlock = false;
+      }
+    }
+  }
 }
