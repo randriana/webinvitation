c9447c25f8e491d4d3b882c626d78d5b589231ab
Rina Andriana <rina.andriana@knowit.no>
2018-02-11 14:21:08 +0100

    Fix stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
     return gulp.src('./src/app/**/*.scss')
-    //.pipe(autoprefixer())
       .pipe(sass().on('error', sass.logError))
       .pipe(gulp.dest(function ( file ) {
         return file.base;
