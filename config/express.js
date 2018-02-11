3d6c6808ad537d58d2bf91dc25da557b49cdd3c4
Rina Andriana <rina.andriana@knowit.no>
2018-02-11 21:01:51 +0100

    Redirect when 404

--- config/express.js
+++ config/express.js
@@ -28,8 +28,12 @@ module.exports = function ( app ) {
         res.locals.message = err.message;
         res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-        // render the error page
-        res.status(err.status || 500);
-        res.send(err.message);
+        if ( err.status === 404 ) {
+          res.redirect('/');
+        } else {
+          // render the error page
+          res.status(err.status || 500);
+          res.send(err.message);
+        }
     });
 }
